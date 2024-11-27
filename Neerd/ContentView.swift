410a48058e857ec063ec91090d14863cdf8550cb
koddaz <Albert.niklasson@gmail.com>
2024-11-27 21:59:30 +0100

    Initial Commit

---
+++ Neerd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Neerd
+//
+//  Created by Albert Niklasson on 2024-11-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Neerd/NeerdApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeerdApp.swift
+//  Neerd
+//
+//  Created by Albert Niklasson on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct NeerdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
